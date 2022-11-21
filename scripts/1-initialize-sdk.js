bb2046c0eed584ffbfe6fc2226061f9a778eef10
Ivan <ivan_leong_97@hotmail.com>
2022-11-21 17:14:46 +0800

    Add connect to wallet; Initialize SDK

---
+++ scripts/1-initialize-sdk.js
@@ -0,0 +1,33 @@
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+import dotenv from "dotenv";
+dotenv.config();
+
+if (!process.env.PRIVATE_KEY || process.env.PRIVATE_KEY === "") {
+    console.log("Private key not found");
+}
+
+if (!process.env.ALCHEMY_API_URL || process.env.ALCHEMY_API_URL === "") {
+    console.log("Alchemy API URL not found");
+}
+
+if (!process.env.WALLET_ADDRESS || process.env.WALLET_ADDRESS === "") {
+    console.log("Wallet Address not found");
+}
+
+const sdk = ThirdwebSDK.fromPrivateKey(
+    process.env.PRIVATE_KEY,
+    process.env.ALCHEMY_API_URL
+);
+
+(async () => {
+    try {
+        const address = await sdk.getSigner().getAddress();
+        console.log("SDK initialized by address: ", address);
+    } catch (err) {
+        console.error("Failed to get apps from the sdk", err);
+        process.exit(1);
+    }
+})();
+
+export default sdk
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
+
 const App = () => {
+  const address = useAddress();
+  console.log("Address: ", address);
+
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to MapleDAO</h1>
+        <div className="btn-hero">
+          <ConnectWallet />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>Wallet connected!</h1>
     </div>
-  );
+  )
+
 };
 
 export default App;
